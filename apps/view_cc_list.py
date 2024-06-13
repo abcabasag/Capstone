959b968aed84de7f210b3e584cb2b120363570a2
abcabasag <151675191+abcabasag@users.noreply.github.com>
2024-06-13 20:18:24 +0800

    Update view_cc_list.py

--- apps/view_cc_list.py
+++ apps/view_cc_list.py
@@ -1,7 +1,7 @@
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
-import dash_html_components as html
+from dash import html
 import pandas as pd
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
