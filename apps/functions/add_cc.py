11aff1cdd1b04d769d29b81d5b02f80451dde734
jommilay <156973035+jommilay@users.noreply.github.com>
2024-06-14 14:06:05 +0800

    Update add_cc.py

--- apps/functions/add_cc.py
+++ apps/functions/add_cc.py
@@ -123,7 +123,8 @@ layout = html.Div(
             ],
             centered=True,
             id='add_cc_successmodal',
-            backdrop='static'
+            backdrop='static',
+            hide_close_button=True  # Add this line to hide the close button
         )
     ],
     style={"maxWidth": "1600px", "margin-left": "220px"}
